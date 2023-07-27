a31f29584b36c0545eb3cf7647945aee959ca2ec
nabeel <nabeelnabu937@gmail.com>
2023-07-27 21:28:09 +0530

    first

---
+++ ecommerceproject/ecommerceapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+app_name='ecommerceapp'
+
+urlpatterns=[
+    path('',views.allProCat,name='allProCat'),
+    path('<slug:c_slug>/',views.allProCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='proDetail'),
+
+]
